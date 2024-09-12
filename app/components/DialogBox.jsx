0546dfcff4a43231e5a0a17e844ffee2e226df38
lucianodourado <luciano.dourado@mpt.mp.br>
2024-09-12 12:38:33 -0300

    wip

---
+++ app/components/DialogBox.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const DialogBox = ({ isOpen, onClose, title, children, autoCloseTime = 3000 }) => {
+  React.useEffect(() => {
+    // Se o dialog estiver aberto, iniciar o timer
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        onClose();  // Fechar o Dialog automaticamente após o tempo definido
+      }, autoCloseTime);
+
+      // Limpar o timer se o dialog for fechado manualmente antes do tempo
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen, onClose, autoCloseTime]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white rounded-lg shadow-lg w-1/3 p-6">
+        {/* Título da Dialog */}
+        <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+
+        {/* Conteúdo da Dialog */}
+        <div className="mb-6">
+          {children}
+        </div>
+
+        {/* Botões de controle */}
+        <div className="flex justify-end space-x-4">
+          <button
+            className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
+            onClick={onClose}
+          >
+            Fechar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DialogBox;
---
+++ app/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+//import Image from "next/image";
+import React from 'react';
+
+
+import DialogBox from '@/app/components/DialogBox';
+
+const Botao = ({ clickHandle }) => {
+
+  return (
+    <div>
+
+      <button className="btn-redondo-volume" onClick={clickHandle}>
+        ok
+      </button>
+    </div>
+  );
+};
+const Main = () => {
+  const [frase, setFrase] = React.useState("");
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+
+  const openDialog = () => setIsDialogOpen(true);
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const clickHandle = async () => {
+    const f= await (await fetch('/api')).json()
+    setFrase(f.message);
+    openDialog();
+  };
+  return (
+    <div class="container">
+      <div style={{ marginBotton: '4px' }}>
+        <Botao clickHandle={clickHandle} />
+      </div>
+      <DialogBox isOpen={isDialogOpen} onClose={closeDialog} title="Vida minimalista">
+        <p>{frase}</p>
+      </DialogBox>
+    </div>
+  );
+};
+
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Main />
+    </div>
+  );
+}

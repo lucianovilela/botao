7a841aded68d5eb753167ff8945afb47ccb67545
lucianovilela <lucianovilela@gmail.com>
2024-09-23 20:22:33 +0000

    dark mode

--- app/components/DialogBox.jsx
+++ app/components/DialogBox.jsx
@@ -17,26 +17,29 @@ const DialogBox = ({ isOpen, onClose, title, children, autoCloseTime = 5000 }) =
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg shadow-lg  p-6">
-        {/* Título da Dialog */}
-        <h2 className="text-2xl font-semibold mb-4">{title}</h2>
-
-        {/* Conteúdo da Dialog */}
-        <div className="mb-6">
-          {children}
-        </div>
-
-        {/* Botões de controle */}
-        <div className="flex justify-end space-x-4">
-          <button
-            className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
-            onClick={onClose}
-          >
-            Fechar
-          </button>
-        </div>
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6">
+      {/* Título da Dialog */}
+      <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
+        {title}
+      </h2>
+  
+      {/* Conteúdo da Dialog */}
+      <div className="mb-6 text-gray-900 dark:text-gray-200">
+        {children}
+      </div>
+  
+      {/* Botões de controle */}
+      <div className="flex justify-end space-x-4">
+        <button
+          className="px-4 py-2 bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-gray-200 rounded-md hover:bg-gray-400 dark:hover:bg-gray-600"
+          onClick={onClose}
+        >
+          Fechar
+        </button>
       </div>
     </div>
+  </div>
+  
   );
 };
 

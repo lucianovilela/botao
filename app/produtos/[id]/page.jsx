bb0f9d951af8aadb6f79edaed2f2a2bd5847f6b9
lucianovilela <lucianovilela@gmail.com>
2024-10-15 22:11:01 +0000

    paginacao

--- app/produtos/[id]/page.jsx
+++ app/produtos/[id]/page.jsx
@@ -10,39 +10,46 @@ export async function generateStaticParams() {
 
 const Page = ({ params }) => {
     return (
-        <div className="min-h-screen ">
+        <div className="min-h-screen bg-gray-100">
             <div className="container mx-auto p-6">
-                <h1 className="text-4xl font-bold text-center mb-8">Produto Recomendado</h1>
+                <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">
+                    Produto Recomendado
+                </h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {products.map((product) => {
-                        {
-                            return product.id === params.id ?
-
-                                < div key={product.id} className=" shadow-lg rounded-lg overflow-hidden" >
-                                    <img
-                                        className="w-full h-48 object-cover"
-                                        src={product.image}
-                                        alt={product.name}
-                                    />
-                                    <div className="p-4">
-                                        <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-                                        <p className=" mb-4">{product.description}</p>
-                                        <a
-                                            href={product.affiliateLink}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-                                        >
-                                            Ver na Amazon
-                                        </a>
-                                    </div>
+                        return product.id === params.id ? (
+                            <div
+                                key={product.id}
+                                className="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-300 hover:scale-105"
+                            >
+                                <img
+                                    className="w-full h-56 object-cover"
+                                    src={product.image}
+                                    alt={product.name}
+                                />
+                                <div className="p-6">
+                                    <h2 className="text-2xl font-semibold mb-2 text-gray-900">
+                                        {product.name}
+                                    </h2>
+                                    <p className="mb-4 text-gray-700 line-clamp-3">
+                                        {product.description}
+                                    </p>
+                                    <a
+                                        href={product.affiliateLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="inline-block bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition-colors duration-200"
+                                    >
+                                        Ver na Amazon
+                                    </a>
                                 </div>
-                                : undefined
-                        }
+                            </div>
+                        ) : null;
                     })}
                 </div>
             </div>
-        </div >
+        </div>
+
     );
 };
 
--- app/produtos/page.jsx
+++ app/produtos/page.jsx
@@ -1,30 +1,61 @@
 
+"use client"
 import products from "@/app/components/Produts"
 import Link from 'next/link'
+import { useState } from "react";
 
-const AffiliateProducts = () => {
+const ProdutosRecomendados = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 1;
+
+  // Calcula o índice do primeiro e do último item da página atual
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  // Seleciona os produtos a serem exibidos na página atual
+  const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Calcula o número total de páginas
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  // Função para trocar de página
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto p-6">
-        <h1 className="text-4xl font-bold text-center mb-8">Produtos Recomendados</h1>
+        <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">
+          Produtos Recomendados
+        </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {products.map((product) => (
-            <div key={product.id} className=" shadow-lg rounded-lg overflow-hidden">
-              <img
-                className="w-full h-48 object-cover"
-                src={product.image}
-                alt={product.name}
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+          {currentProducts.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
+            >
+              <div className="relative h-64">
+                <img
+                  className="absolute inset-0 w-full h-full object-cover"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </div>
+              <div className="p-6">
+                <h2 className="text-2xl font-semibold mb-2 text-gray-900">
+                  {product.name}
+                </h2>
                 <Link href={`produtos/${product.id}`}>
-                  <p className=" mb-4">{product.description}</p>
+                  <p className="mb-4 text-gray-700 line-clamp-3">
+                    {product.description}
+                  </p>
                 </Link>
                 <a
                   href={product.affiliateLink}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                  className="inline-block bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition-colors duration-200"
                 >
                   Ver na Amazon
                 </a>
@@ -32,9 +63,25 @@ const AffiliateProducts = () => {
             </div>
           ))}
         </div>
+        {/* Controles de Paginação */}
+        <div className="flex justify-center mt-8">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={`mx-1 px-4 py-2 rounded-full ${
+                currentPage === index + 1
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-300 text-gray-700 hover:bg-gray-400"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default AffiliateProducts;
+export default ProdutosRecomendados;

caf594edbf602b6383235d3043786fe146bd35dc
lucianovilela <lucianovilela@gmail.com>
2024-10-16 19:35:53 +0000

    paisagem

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,20 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        {/* Imagem de fundo difusa */}
+        <div
+          className="fixed inset-0 bg-center bg-cover filter blur-sm"
+          style={{ backgroundImage: "url('https://image.pollinations.ai/prompt/uma%20imagem%20calma%20de%20uma%20paisagem%20photorealista%20ao%20amanhecer%20com%20animais%20e%20uma%20cachoeira')" }}
+        ></div>
 
-
+        {/* Conteúdo principal */}
+        <div className="relative z-10 flex items-center justify-center min-h-screen">
+          <div className="">
+            {children}
+          </div>
+        </div>
 
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJXQB2G7"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>

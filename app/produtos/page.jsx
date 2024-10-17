11a40ee6197d6e97095019960ef8cffb2305a6a0
lucianovilela <lucianovilela@gmail.com>
2024-10-17 20:29:32 +0000

    bug layout

--- app/produtos/page.jsx
+++ app/produtos/page.jsx
@@ -1,46 +1,31 @@
 
-"use client"
+
 import products from "@/app/components/Produts"
 import Link from 'next/link'
-import { useState } from "react";
+
 
 const ProdutosRecomendados = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
 
-  // Calcula o índice do primeiro e do último item da página atual
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-
-  // Seleciona os produtos a serem exibidos na página atual
-  const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
-
-  // Calcula o número total de páginas
-  const totalPages = Math.ceil(products.length / itemsPerPage);
-
-  // Função para trocar de página
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
 
   return (
+    
     <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto p-6">
+      <div className="mx-auto p-6">
         <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">
           Produtos Recomendados
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {currentProducts.map((product) => (
+          {products.map((product) => (
             <div
-              key={product.id}
-              className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
+            key={product.id}
+            className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
             >
               <div className="relative h-64">
                 <img
                   className="absolute inset-0 w-full h-full object-cover"
                   src={product.image}
                   alt={product.name}
-                />
+                  />
               </div>
               <div className="p-6">
                 <h2 className="text-2xl font-semibold mb-2 text-gray-900">
@@ -56,31 +41,17 @@ const ProdutosRecomendados = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-block bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition-colors duration-200"
-                >
+                  >
                   Ver na Amazon
                 </a>
               </div>
             </div>
           ))}
         </div>
-        {/* Controles de Paginação */}
-        <div className="flex justify-center mt-8">
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button
-              key={index}
-              onClick={() => handlePageChange(index + 1)}
-              className={`mx-1 px-4 py-2 rounded-full ${
-                currentPage === index + 1
-                  ? "bg-blue-600 text-white"
-                  : "bg-gray-300 text-gray-700 hover:bg-gray-400"
-              }`}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </div>
+
       </div>
     </div>
+        
   );
 };
 
